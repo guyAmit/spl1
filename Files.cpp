3965505596ee061a7c9f7413c9a1b8e8aefcbcbd
nadav <nadavbr5@gmail.com>
2017-11-08 14:21:44 -0500

    implemented BaseFile and File

--- Files.cpp
+++ Files.cpp
@@ -3,3 +3,141 @@
 //
 
 #include "Files.h"
+
+//BaseFile class
+BaseFile::BaseFile(string name) : name("default") {
+    if (baseFile_Valid_Name(name))
+        this->name = name;
+    else {
+        throw std::exception();
+    }
+}
+
+string BaseFile::getName() const { return name; }
+
+void BaseFile::setName(string newName) { name = newName; }
+
+bool BaseFile::baseFile_Valid_Name(const string &name) {
+    return (name.find_first_not_of("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") ==
+            std::string::npos);
+}
+
+//File class
+File::File(string name, int size) : BaseFile(name), size(0) {
+    if (size <= 0) {
+        throw std::exception();
+    } else {
+        this->size = size;
+    }
+}
+
+int File::getSize() { return size; }
+
+//Directory class
+//todo: add rule of 5 methods
+Directory::Directory(string name, Directory *parent) : BaseFile(name), parent(nullptr) {
+    if (parent) {
+        this->parent = parent;
+        this->children
+//        this->children = new vector();
+    } else {
+        throw std::exception();
+    }
+}
+
+Directory *Directory::getParent() const {
+    return parent;
+}
+
+void Directory::setParent(Directory *newParent) {
+    if (newParent) {
+        parent = newParent;
+    } else {
+        throw std::exception();
+    }
+}
+
+void Directory::addFile(BaseFile *file) {
+    if (!file)
+        throw std::exception();
+    if (searchFileName(file)) {
+        children.push_back(file);
+    } else {
+        throw std::exception();
+    }
+
+}
+
+Directory::~Directory() {
+    clean();
+}
+
+
+void Directory::clean() const {
+    for (auto baseFile :children) {
+        delete baseFile;
+    }
+}
+
+
+bool Directory::searchFileName(BaseFile *file) {
+    if (children.empty()) {
+        return true;
+    } else {//iterate through the vector and find a matching file name
+        auto it = std::find_if(children.begin(), children.end(), [](
+                const BaseFile &baseFile) -> bool { return baseFile.getName() == file->getName(); });
+        return it == children.end();
+    }
+}
+
+void Directory::removeFile(string name) {
+
+}
+
+void Directory::removeFile(BaseFile *file) {
+
+}
+
+void Directory::sortByName() {
+
+}
+
+void Directory::sortBySize() {
+
+}
+
+vector<BaseFile *> Directory::getChildren() {
+    return vector<BaseFile *>();
+}
+
+int Directory::getSize() {
+    int sumSize = 0;
+    for ()
+}
+
+string Directory::getAbsolutePath() {
+    if (!parent)
+        return getName();
+    else
+        return parent->getAbsolutePath() + getName();
+}
+
+ vector<BaseFile*> Directory::copy()const {
+    auto newVector = new vector();
+    for (auto baseFile :children) {
+        //todo: the vectors have the same baseFiles but maybe in different order by default.
+        newVector->push_back(baseFile);
+    }
+    return newVector;
+}
+
+Directory &Directory::operator=(const Directory &rhs) {
+    if (this != &rhs) {
+        clean();
+        this->parent = rhs.parent;
+        this->children = rhs.copy();
+
+    }
+}
+
+
--- Files.h
+++ Files.h
@@ -3,16 +3,30 @@
 
 #include <string>
 #include <vector>
+#include <algorithm>
+
+using std::string;
+using std::vector
 
 class BaseFile {
 private:
     string name;
 
+/**
+ *
+ * @param name
+ * @return true iff name contains digits or letters only.
+ */
+    bool baseFile_Valid_Name(const string &name);
+
 public:
     BaseFile(string name); //todo:: create test for file name
     string getName() const;
+
     void setName(string newName);
+
     virtual int getSize() = 0;
+
 
 };
 
@@ -29,8 +43,24 @@ public:
 
 class Directory : public BaseFile {
 private:
-    vector<BaseFile*> children; //this is a list of both files and folders
+    vector<BaseFile *> children; //this is a list of both files and folders
     Directory *parent;
+
+/**
+ *
+ * @param file
+ * @return true iff there is no file with the same name as the given file
+ */
+    bool searchFileName(BaseFile *file);
+    /**
+     * delete all the pointers in children
+     */
+    void clean() const;
+    /**
+     * deep copies this children vector into new vector.
+     * @return a pointer to the new vector
+     */
+    vector<BaseFile*> copy()const;
 
 public:
     Directory(string name, Directory *parent); // Constructor
@@ -40,22 +70,25 @@ public:
 
     void setParent(Directory *newParent); // Change the parent of this directory
     //todo:: null check parent
-    void addFile(BaseFile* file); // Add the file to children
+    void addFile(BaseFile *file); // Add the file to children
     //todo:: create add\delete tests
     void removeFile(string name); // Remove the file with the specified name from children
 
 
-    void removeFile(BaseFile* file); // Remove the file from children
+    void removeFile(BaseFile *file); // Remove the file from children
 
 
     //todo:: import a generic sort algortim
     void sortByName(); // Sort children by name alphabetically (not recursively)
     void sortBySize(); // Sort children by size (not recursively)
 
-    vector<BaseFile*> getChildren(); // Return children
+    vector<BaseFile *> getChildren(); // Return children
     int getSize(); // Return the size of the directory (recursively)
-    string getAbsolutePath();  //Return the path from the root to this(recursively)
+    string getAbsolutePath();//Return the path from the root to this(recursively)
+    virtual ~Directory();
 
+
+    Directory &operator=(const Directory &rhs);
 };
 
 #endif
