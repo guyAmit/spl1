b1237a91c14f8d05f46772872792edbd7f2f2ce5
Guy Amit <guypguy1@gmail.com>
2017-11-11 10:34:07 +0200

    implamented FileSystem, waiting for tests

--- FileSystem.cpp
+++ FileSystem.cpp
@@ -3,3 +3,72 @@
 //
 
 #include "FileSystem.h"
+#include "string"
+
+using namespace std;
+
+FileSystem::FileSystem() {
+    this->rootDirectory=new Directory("/", nullptr);
+    this->workingDirectory=rootDirectory;
+}
+
+Directory& FileSystem::getRootDirectory() const {
+    return this->*rootDirectory;
+}
+
+Directory& FileSystem::getWorkingDirectory() const {
+    return this->*workingDirectory;
+}
+
+void FileSystem::setWorkingDirectory(Directory *newWorkingDirectory) {
+    if(!newWorkingDirectory) {
+        this->workingDirectory = newWorkingDirectory;
+    }
+    else{throw std::exception();}
+}
+/************************************************
+for test perpesese only::implamention of rule of 5
+ there should be only one file system.
+*************************************************/
+
+void FileSystem::clean() const {
+    delete this->rootDirectory;
+    delete this->workingDirectory;
+}
+
+void FileSystem::copy(const FileSystem &rhs) {
+    this->rootDirectory=rhs.rootDirectory;
+    this->workingDirectory=rhs.workingDirectory;
+
+}
+
+void FileSystem::steal(FileSystem &rhs) {
+    this->rootDirectory=rhs.rootDirectory;
+    this->workingDirectory=rhs.workingDirectory;
+    rhs.workingDirectory= nullptr;
+    rhs.rootDirectory= nullptr;
+}
+
+FileSystem::FileSystem(const FileSystem &rhs) {
+    copy(rhs);
+}
+
+FileSystem::~FileSystem() {
+    clean();
+}
+
+FileSystem& FileSystem::operator=(FileSystem &&rhs) {
+    clean();
+    steal(rhs);
+    return *this;
+}
+
+FileSystem& FileSystem::operator=(const FileSystem &rhs) {
+    if(this != &rhs){
+        clean();
+        copy(rhs);
+    }
+    return *this;
+}
+
+
--- FileSystem.h
+++ FileSystem.h
@@ -8,12 +8,23 @@ class FileSystem {
 private:
     Directory* rootDirectory;
     Directory* workingDirectory;
+
+    //rule of 5 methods
+    void clean() const ;
+    void steal(FileSystem &rhs);
+    void copy(const FileSystem &rhs);
+
 public:
     FileSystem();
     Directory& getRootDirectory() const; // Return reference to the root directory
     Directory& getWorkingDirectory() const; // Return reference to the working directory
     void setWorkingDirectory(Directory *newWorkingDirectory); // Change the working directory of the file system
 
+    //rule of 5 methods
+    FileSystem &operator=(const FileSystem  &rhs);
+    FileSystem &operator=(FileSystem &&rhs);
+    FileSystem(const FileSystem &rhs);
+    ~FileSystem();
 
 };
 
